9a7ae3e2a70750ce639a31940f3bf885497e2ea5
Akua Apeagyei <apeagyei.akua@gmail.com>
2019-04-01 14:47:24 +0100

    Installed shotgun function and add cat image and border

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+  'hello!'
+end
+
+get '/secret' do
+  'bye felicia'
+end
+
+get'/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
